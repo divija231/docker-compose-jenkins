d2b58d30da5920ef5aba959f1492d795e31f8561
divija231 <142985996+divija231@users.noreply.github.com>
2024-01-04 12:55:29 +0530

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+pipeline{
+  agent none
+  environment{
+    GIT = 'https://github.com/divija231/raow.git'
+    BRANCH ='master'
+    
+  }
+  stages{
+    stage("clonning from git"){
+      steps{
+        node { label 'jenkins'}
+        sh "ls"
+        git url : GIT , branch : BRANCH
+        sh "docker-compose -f docker-compose.yaml up -d "
+        sh "ls "
+        sh "docker ps -aq"        
+      }
+    }
+    stage("checking"){
+      agent any 
+      steps{
+        sh "docker images"
+      }
+    }
+  }
+}
